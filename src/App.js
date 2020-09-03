c16f7004876511c8a694ae4eaf8bea9f12c79517
Johan Bijlsma <johan.bijlsma24@gmail.com>
2020-09-03 14:09:38 +0200

    added more examples

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 
+import React, { useState } from 'react';
 import { animated, useSpring } from 'react-spring';
 
-import React from 'react';
+import TransitionExample from './transition';
 
 function App() {
 	const colorProps = useSpring({
@@ -12,21 +13,40 @@ function App() {
 
 	const numberProps = useSpring({ number: 100, from: { number: 0 } });
 	const scaleProps = useSpring({ height: 200, from: { height: 0 } });
-
+	const [isToggled, setToggle] = useState(false);
+	const toggleProps = useSpring({
+		transform: isToggled ? 'scale(1)' : 'scale(0)',
+	});
+	const togglePropsReversed = useSpring({
+		transform: isToggled ? 'scale(0)' : 'scale(1)',
+	});
 	return (
 		<div className='App'>
 			<animated.header className='App-header' style={colorProps}>
 				<h1>React Spring</h1>
-				<animated.img
-					src='https://i.imgur.com/QZownhg.png'
-					alt='logo React Spring'
-					style={scaleProps}
-				/>
-				<p>
-					Fake loader:
-					<br />
-					<animated.span>{numberProps.number}</animated.span>%
-				</p>
+				<animated.section className='example1' style={togglePropsReversed}>
+					<animated.img
+						src='https://i.imgur.com/QZownhg.png'
+						alt='logo React Spring'
+						style={scaleProps}
+					/>
+					<p>
+						Fake loader:
+						<br />
+						<animated.span>{numberProps.number}</animated.span>
+					</p>
+				</animated.section>
+				<animated.section className='example2' style={toggleProps}>
+					<TransitionExample />
+				</animated.section>
+
+				<button
+					onClick={() =>
+						setToggle(!isToggled) && console.log('button got clicked')
+					}
+				>
+					Next example ⟩
+				</button>
 			</animated.header>
 		</div>
 	);
---
+++ src/transition.js
@@ -0,0 +1,40 @@
+import './transition.css';
+
+import React, { useCallback, useState } from 'react';
+import { animated, useTransition } from 'react-spring';
+
+import { render } from 'react-dom';
+
+const pages = [
+	({ style }) => (
+		<animated.div style={{ ...style, background: 'lightpink' }}>A</animated.div>
+	),
+	({ style }) => (
+		<animated.div style={{ ...style, background: 'lightblue' }}>B</animated.div>
+	),
+	({ style }) => (
+		<animated.div style={{ ...style, background: 'lightgreen' }}>
+			C
+		</animated.div>
+	),
+];
+
+export default function TransitionExample() {
+	const [index, set] = useState(0);
+	const onClick = useCallback(() => set((state) => (state + 1) % 3), []);
+	const transitions = useTransition(index, (p) => p, {
+		from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+		enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+		leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+	});
+	return (
+		<div className='simple-trans-main' onClick={onClick}>
+			{transitions.map(({ item, props, key }) => {
+				const Page = pages[item];
+				return <Page key={key} style={props} />;
+			})}
+		</div>
+	);
+}
+
+render(<TransitionExample />, document.getElementById('root'));
